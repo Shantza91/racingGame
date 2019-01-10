c2ba5085738f5e93f6ccf97089d9bf602adeb638
Santa George <santageorge1991@gmail.com>
2019-01-10 19:11:09 +0200

    Studied static variables and constants.

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,27 +4,40 @@ package org.fasttrackit;
 public class App 
 {
     public static void main( String[] args ) {
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        engine.capacity = 1500;
 
-        Car car = new Car(engine);
-        car.setName("Dacia");
-        car.setColor("white");
-        car.doorCount = 4;
-        car.setMileage(7.5);
-        car.running = true;
+        // Example of static variables unique for the whole application
+        Vehicle vehicle1 = new Vehicle();
+        vehicle1.vehicleCount = 1;
 
-        double carTraveledDistance = car.accelerate(100, 2);
+        Vehicle vehicle2 = new Vehicle();
+        vehicle2.vehicleCount = 2;
 
-        car.engine = engine;
+        Vehicle.vehicleCount = 3;
 
-        car.engine.manufacturer = "BMW";
-        engine = null;
-
-
-        Car car2 = new Car(engine);
-        car2.setName("sfa");
+        System.out.println("Value from vehicle1: "+vehicle1.vehicleCount);
+        System.out.println("Value from vehicle2: "+vehicle2.vehicleCount);
+        System.out.println("Value from Vehicle Class: " +Vehicle.vehicleCount);
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//        engine.capacity = 1500;
+//
+//        Car car = new Car(engine);
+//        car.setName("Dacia");
+//        car.setColor("white");
+//        car.doorCount = 4;
+//        car.setMileage(7.5);
+//        car.running = true;
+//
+//        double carTraveledDistance = car.accelerate(100, 2);
+//
+//        car.engine = engine;
+//
+//        car.engine.manufacturer = "BMW";
+//        engine = null;
+//
+//
+//        Car car2 = new Car(engine);
+//        car2.setName("sfa");
 
     }
 }
