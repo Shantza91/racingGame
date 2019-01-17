09011f2b5a2156e3492c17c51e78c9eb40415461
Santa George <santageorge1991@gmail.com>
2019-01-17 20:34:22 +0200

    Studied for loops.

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -21,9 +21,23 @@ public class Game {
 
     }
 
-    private void displayAvailableTracks(){
+    private void displayAvailableTracks() {
         System.out.println("Available tracks: ");
-        System.out.println(tracks[0].getName());
-        System.out.println(tracks[1].getName());
+
+        for (int i = 0; i < tracks.length; i++) {
+            // if object is different than null
+            if (tracks[i] != null) {
+                System.out.println(tracks[i].getName());
+            }
+
+            //        System.out.println(tracks[0].getName());
+            //        System.out.println(tracks[1].getName());
+        }
+        // example of enhanced for / "for-each"
+        for (Track track : tracks){
+            if(track != null) {
+                System.out.println(track.getName());
+            }
+        }
     }
 }
